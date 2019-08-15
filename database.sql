da8414b2cc3f2052979f2c4fb8206fc3c7cedd5e
raysilent7 <rgc471@hotmail.com>
2019-08-15 16:21:58 -0300

    new database.sql script

--- database.sql
+++ database.sql
@@ -1,14 +1,21 @@
-CREATE DATABASE `libraryjdbc` 
-/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ 
-/*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `book` 
+(   `Id` int(4) NOT NULL AUTO_INCREMENT,   
+`Isbn` int(4) NOT NULL,   
+`Name` varchar(60) DEFAULT NULL,   
+`Autor` varchar(60) DEFAULT NULL,   
+`Price` float DEFAULT NULL,   
+`ReleaseDate` date DEFAULT NULL,   
+`ImgPath` varchar(60) DEFAULT NULL,   
+PRIMARY KEY (`Id`),   
+UNIQUE KEY `Isbn` (`Isbn`) ) 
+ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `book` (
-  `Id` int(7) NOT NULL AUTO_INCREMENT,
-  `Isbn` int(11) NOT NULL,
-  `Name` varchar(60) DEFAULT NULL,
-  `Autor` varchar(60) DEFAULT NULL,
-  `Price` float DEFAULT NULL,
-  `ReleaseDate` date DEFAULT NULL,
-  `ImgPath` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`Id`, `Isbn`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `libraryjdbc`.`book` 
+VALUES (1, 100000, 'Far Away Lands', 'Rayan Garcia da Cunha',  
+29.9, '2018-12-31', '/home/rgarcia/Pictures/bazelplush.jpg'), 
+(2, 100001, 'Shadow and Bones', 'Leigh Bardugo',  
+39.9, '2010-01-09', '/home/rgarcia/Pictures/bazelplush.jpg'), 
+(3, 100002, 'Siege and Storm', 'Leigh Bardugo',  
+59.9, '2012-05-23', '/home/rgarcia/Pictures/teste.jpg'), 
+(4, 100003, 'Ruin and Ascension', 'Leigh Bardugo',  
+59.9, '2015-03-24', '/home/rgarcia/Pictures/teste.jpg');
